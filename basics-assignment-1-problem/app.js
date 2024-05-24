ffee374447d588c8dcd01f83327d925315bba7ac
NGUYEN THI BINH AN <an.ntb@lgcns.com>
2024-05-23 19:37:10 -0700

    assigment 1

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: 'NGUYEN THI BINH AN',
+            age: 30,
+            imageUrl: 'my-painting.jpg'
+        };
+    }
+});
+
+app.mount('#assignment');
