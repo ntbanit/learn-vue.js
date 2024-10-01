3872131613fd8867b844a8c2801df521ffe1ab61
NGUYEN THI BINH AN <ntban.it@gmail.com>
2024-10-01 11:39:03 +0700

    section 9 part 1 - config git in new local env.

--- section-09-component-deep-dive/cmp-adv-01-starting-setup/src/components/BadgeList.vue
+++ section-09-component-deep-dive/cmp-adv-01-starting-setup/src/components/BadgeList.vue
@@ -1,22 +1,29 @@
 <template>
-  <section>
-    <h2>Available Badges</h2>
-    <ul>
-      <li>
-        <base-badge type="admin" caption="ADMIN"></base-badge>
-      </li>
-      <li>
-        <base-badge type="author" caption="AUTHOR"></base-badge>
-      </li>
-    </ul>
-  </section>
+  <base-card>
+    <!-- 
+    <template v-slot:header>
+      <h2>Available Badges</h2>
+    </template> 
+    -->
+    <template #default>
+      <ul>
+        <li>
+          <base-badge type="admin" caption="ADMIN"></base-badge>
+        </li>
+        <li>
+          <base-badge type="author" caption="AUTHOR"></base-badge>
+        </li>
+      </ul>
+    </template>
+  </base-card>
 </template>
 
-<style>
-section h2 {
+<style scoped>
+h2 {
   margin: 0.5rem 0;
   color: #3a3a3a;
 }
+
 ul {
   list-style: none;
   margin: 0;
