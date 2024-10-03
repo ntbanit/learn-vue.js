bf9616a6840df2edc1ac7b26c3bdada1a7894d14
NGUYEN THI BINH AN <ntban.it@gmail.com>
2024-10-03 11:59:49 +0700

    section 10 video 134

---
+++ section-10-learning-resource-app/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <TheHeader title="Remember Me"></TheHeader>
+    <StoredResources :resources="storedResources"></StoredResources>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+export default {
+    components: {
+        TheHeader,
+        StoredResources
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'udemy',
+                    title: 'LG CNS Udemy',
+                    description: 'Learning Courses that provided by company',
+                    link: 'https://lg-cns.udemy.com/'
+                },
+                {
+                    id: 'youtube',
+                    title: 'Youtube',
+                    description: 'Online videos that share publicly',
+                    link: 'https://www.youtube.com/'
+                }
+            ]
+        };
+    },
+    mounted() {
+        // console.log(this.storedResources);
+    }
+
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Jost', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
---
+++ section-10-learning-resource-app/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+        <LearningResource v-for="res in resources" :key="res.id" :resource="res">
+        </LearningResource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+    components: {
+        LearningResource
+    },
+    props: ['resources']
+
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
